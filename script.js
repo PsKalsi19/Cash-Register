d46bbe721362c466bedd0986189fdfd1ab6663f8
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2022-09-05 22:04:59 +0530

    Feat: Get the stuff from HTML to JavaScript land.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Checking if script is working');
+// For input fields
+let billAmount = document.querySelector("#bill-amount");
+let cashReceived = document.querySelector("#cash-received");
+let errorMessageContainer = document.querySelector("#error");
+let processButton = document.querySelector("#process-button");
+
+processButton.addEventListener("click",process);
+
+function process() {
+  console.log(billAmount.value)
+  console.log(cashReceived.value)
+}
